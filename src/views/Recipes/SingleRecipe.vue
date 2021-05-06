82bcccc5029f782f78d5127d5880f84dbfe45d5e
Larissa129 <10603084@my.uvu.edu>
2021-05-06 13:39:55 -0600

    Favorite add remove

--- src/views/Recipes/SingleRecipe.vue
+++ src/views/Recipes/SingleRecipe.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="single-recipe">
+    <!-- snackbar  start -->
+    <v-container class="text-center">
+      <v-snackbar centered v-model="snackbar">
+        {{ snackbarText }}
+        <template v-slot:action="{ attrs }">
+          <v-btn color="primary" text v-bind="attrs" @click="snackbar = false">
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+    </v-container>
+    <!-- snackbar end  -->
     <v-btn
       v-show="upFab"
       v-scroll="onScroll"
@@ -15,23 +27,27 @@
     </v-btn>
 
     <v-container> </v-container>
-    <v-card class="mx-auto " max-width="900">
+    <v-card class="mx-auto" max-width="900">
       <v-container class="my-10 container">
         <v-layout row>
           <v-flex>
-            <v-card-title
-              class="justify-start grey--text h2--text mt-5 display-1"
-              >{{ recipe.name }}</v-card-title
+            <v-container class="review">
+              <v-card-title
+                class="justify-start grey--text h2--text mt-5 display-1"
+                >{{ recipe.name }}</v-card-title
+              >
+              <v-row>
+                <v-col cols="12" md="12" lg="12">
+                  <DisplayRating :id="id" />
+                </v-col>
+              </v-row>
+            </v-container>
+            <v-card-subtitle class="offset-1 text--grey"
+              >Created By: {{ username }}</v-card-subtitle
             >
-            <v-row>
-              <v-col cols="12" md="12" lg="12">
-                <DisplayRating :id="id" />
-              </v-col>
-            </v-row>
-            <!-- <v-card-subtitle class="offset-1 text--grey" >Created By: {{recipe.author.username}}</v-card-subtitle> -->
             <v-img
               max-height="400"
-              max-width="600"
+              max-width="700"
               :src="$store.state.getImageUrl + recipe.imageUrl"
               class="mx-auto my-5"
             ></v-img>
@@ -52,10 +68,7 @@
             <v-chip color="success" class="mr-2 my-2" label outlined
               >Total: {{ setTotalTime }}</v-chip
             >
-            <!-- <v-chip color="primary" class="mr-2 my-2"  label outlined @click="printRecipe()">
-                                <v-icon>mdi-printer</v-icon>
-                                <span class="mx-2">Print</span>
-                            </v-chip> -->
+
             <v-btn bottom fab icon @click.prevent="addtoFavorite">
               <v-icon
                 size="35"
@@ -68,64 +81,94 @@
             <!-- </div> -->
           </v-flex>
           <v-divider class="mr-2 my-2 "></v-divider>
-          <h3 class="grey--text my-2 ">Ingredients:</h3>
-          <v-flex class="d-flex justify-start flex-wrap ">
-            <v-list flat>
-              <v-list-item-group multiple>
-                <v-list-item
-                  v-for="(ingredient, index) in ingredients"
-                  :key="index"
-                >
-                  <template v-slot:default="{ active }">
-                    <v-list-item-action>
-                      <v-checkbox :input-value="active"></v-checkbox>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title
-                        >{{ ingredient.whole }} {{ ingredient.measure }}
-                        {{ ingredient.utensil }} {{ ingredient.ingredient }}
-                        {{ ingredient.action }}
-                      </v-list-item-title>
-                    </v-list-item-content>
-                  </template>
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
-          </v-flex>
+          <v-container class="review">
+            <v-row class="my-1">
+              <v-col cols="6" md="6" lg="6">
+                <h3 class="grey--text my-2 ">Ingredients:</h3>
+              </v-col>
 
-          <v-divider class="my-2 "></v-divider>
-          <h3 class="grey--text my-2 ">Instructions:</h3>
-          <v-flex class="d-flex justify-start flex-wrap ">
-            <v-list flat>
-              <v-list-item-group multiple>
-                <v-list-item
-                  v-for="instruction in instructions"
-                  :key="instruction.step"
-                >
-                  <template v-slot:default="{ active }">
-                    <v-list-item-action>
-                      <v-checkbox :input-value="active"></v-checkbox>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <span></span>
-                      <v-list-item-title class="text-wrap"
-                        ><span class="grey--text "
-                          >Step: {{ instruction.step }}</span
-                        >
-                        <p>{{ instruction.instruction }}</p></v-list-item-title
+              <v-col cols="6" md="6" lg="6">
+                <v-card-actions class="justify-end">
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        color="primary"
+                        dark
+                        v-bind="attrs"
+                        v-on="on"
+                        small
+                        @click="addToGroceryList"
                       >
-                    </v-list-item-content>
-                  </template>
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
-          </v-flex>
+                        <v-icon>mdi-plus</v-icon>
+                        <span>To List</span>
+                      </v-btn>
+                    </template>
+                    <span>Send ingredients to grocery list</span>
+                  </v-tooltip>
+                </v-card-actions>
+              </v-col>
+            </v-row>
+
+            <v-flex class="d-flex justify-start flex-wrap ">
+              <v-list flat>
+                <v-list-item-group multiple>
+                  <v-list-item
+                    v-for="(ingredient, index) in ingredients"
+                    :key="index"
+                  >
+                    <template v-slot:default="{ active }">
+                      <v-list-item-action>
+                        <v-checkbox :input-value="active"></v-checkbox>
+                      </v-list-item-action>
+                      <v-list-item-content>
+                        <v-list-item-title
+                          >{{ ingredient.whole }} {{ ingredient.measure }}
+                          {{ ingredient.utensil }} {{ ingredient.ingredient }}
+                          {{ ingredient.action }}
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </template>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-flex>
+
+            <v-divider class="my-2 "></v-divider>
+            <h3 class="grey--text my-2 ">Instructions:</h3>
+            <v-flex class="d-flex justify-start flex-wrap ">
+              <v-list flat>
+                <v-list-item-group multiple>
+                  <v-list-item
+                    v-for="instruction in instructions"
+                    :key="instruction.step"
+                  >
+                    <template v-slot:default="{ active }">
+                      <v-list-item-action>
+                        <v-checkbox :input-value="active"></v-checkbox>
+                      </v-list-item-action>
+                      <v-list-item-content>
+                        <span></span>
+                        <v-list-item-title class="text-wrap"
+                          ><span class="grey--text "
+                            >Step: {{ instruction.step }}</span
+                          >
+                          <p>
+                            {{ instruction.instruction }}
+                          </p></v-list-item-title
+                        >
+                      </v-list-item-content>
+                    </template>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-flex>
+          </v-container>
         </v-layout>
       </v-container>
     </v-card>
     <v-divider></v-divider>
-    <v-container>
-      <div class="display-1 ">Reviews</div>
+    <v-container class="review">
+      <div class="display-1 ">Reviews:</div>
       <Reviews :recipeId="id" />
     </v-container>
   </div>
@@ -134,11 +177,10 @@
 <script>
 import * as recipes from "../../Services/RecipeService";
 import * as user from "../../Services/AuthService";
-import * as review from "../../Services/RatingServices";
 // import Rating from '../Ratings/Rating'
 import DisplayRating from "../Ratings/DisplayRating";
 import Reviews from "../Ratings/Review";
-
+import * as GroceryService from "../../Services/GroceryServices";
 export default {
   name: "SingleRecipe",
   components: {
@@ -152,33 +194,47 @@ export default {
       recipe: {},
       ingredients: [],
       instructions: [],
-      usernname: "",
+      username: "",
       upFab: false,
       selected: false,
+      newItem: [],
+      snackbar: false,
+      snackbarText: "Your items has been added to the grocery list!",
     };
   },
-
   created() {
     recipes.getRecipeById(this.id).then((res) => {
       this.recipe = res.data;
-      console.log(this.recipe.favorite, 111);
       this.selected = this.recipe.favorite;
-      console.log(this.selected, 222);
+      if (this.recipe.author.username) {
+        this.username = this.recipe.author.username;
+      }
       this.ingredients = JSON.parse(res.data.ingredients);
-      console.log(this.ingredients);
       this.instructions = JSON.parse(res.data.instructions);
     });
-
-    review.getRatingById(this.id).then((res) => {
-      console.log(res, 888);
-    });
   },
-
   methods: {
     printRecipe() {
       window.print();
     },
-
+    async addToGroceryList() {
+      if (!user.isLoggedIn()) {
+        this.$router.push({ name: "Login" });
+      }
+      if (this.ingredients.length === 0) {
+        this.snackbarText = "There is not items in this recipe";
+      }
+      const newItems = this.ingredients.map((d) => {
+        return {
+          text: d.ingredient,
+          completed: false,
+        };
+      });
+      for (let item of newItems) {
+        await GroceryService.createGrocery(item);
+      }
+      this.snackbar = true;
+    },
     addtoFavorite: async function() {
       this.selected = !this.selected;
       this.recipe.favorite = this.selected;
@@ -209,7 +265,6 @@ export default {
         recipes.updateFavorite(recipe);
       }
     },
-
     onScroll(e) {
       if (typeof window === "undefined") return;
       const top = window.pageYOffset || e.target.scrollTop || 0;
@@ -263,8 +318,8 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  width: 80%;
+.review {
+  width: 85%;
   height: auto;
 }
 </style>
